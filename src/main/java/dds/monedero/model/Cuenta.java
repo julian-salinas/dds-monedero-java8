221b0da81d508213d216da65acfa685751ef4601
julian-salinas <juliansalinasm@gmail.com>
2022-05-18 23:48:22 -0300

    correccion de code smells en cuenta

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -14,13 +14,16 @@ public class Cuenta {
   // No es necesario inicializar saldo en 0, ya que el valor se inicializa en el constructor de la clase
   private double saldo;
   private List<Movimiento> movimientos = new ArrayList<>();
+  private int limite;
 
   public Cuenta() {
     saldo = 0;
+    limite = 1000;
   }
 
   public Cuenta(double montoInicial) {
     saldo = montoInicial;
+    limite = 1000;
   }
 
   public void setMovimientos(List<Movimiento> movimientos) {
@@ -38,19 +41,13 @@ public class Cuenta {
   }
 
   public void sacar(double cuanto) {
+
     this.validarMontoNegativo(cuanto);
-    if (cuanto <= 0) {
-      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
-    }
-    if (getSaldo() - cuanto < 0) {
-      throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
-    }
-    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
-    double limite = 1000 - montoExtraidoHoy;
-    if (cuanto > limite) {
-      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
-          + " diarios, límite: " + limite);
-    }
+
+    this.validarDineroDisponible(cuanto);
+
+    this.validarMaximoExtraccionDiario(cuanto);
+
     new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
   }
 
@@ -87,6 +84,25 @@ public class Cuenta {
   public void validarCantidadDeDepositos() {
     if (this.getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
+    }
+  }
+
+  public void validarDineroDisponible(double cantidad) {
+    if (this.getSaldo() - cantidad < 0) {
+      throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
+    }
+  }
+
+  public void validarMaximoExtraccionDiario(double cantidad) {
+    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+
+    // en lugar de colocar manualmente el valor 1000, lo agregamos como un atributo límite en la clase Cuenta
+    double limiteDisponible = this.limite - montoExtraidoHoy;
+
+
+    if (cantidad > limiteDisponible) {
+      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
+          + " diarios, límite: " + limiteDisponible);
     }
   }
 }
